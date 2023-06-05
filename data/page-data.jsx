3cfb23eceec8b694dfefea58578dee0a654ba9b8
Zaheer-3 <haniaimam1990@gmail.com>
2023-06-06 00:08:12 +0500

    commit the whole project

---
+++ data/page-data.jsx
@@ -0,0 +1,73 @@
+export const personalData = {
+	name: "Zaheer un Nisa",
+	role: "Software Engineer",
+	education: [
+		"Student :Web.3 Metaverse ,PIAIC.,Batch-42",
+        "M.Ed, Preston university Karachi.(2016-2017)",
+		"B.Ed, Allama Iqbal open University (2004)",
+		"B.A, University of karachi (1996)",
+        
+	],
+	contactLinks: [
+		"mailto:emailto:haniaimam1990@gmail.com",
+	],
+};
+
+export const aboutMe = {
+	title: "About Me",
+	body: [
+		"As a highly motivated and driven individual with a passion for making a positive impact, I have a unique blend of technical and interpersonal skills, thanks to my background in teaching. I am able to tackle  problems with ease, and I never lose sight of my ultimate goal, even when facing challenges.",
+		"When I'm not working, I love cooking, reading, and spending time with my kids and parents. I believe that my determination, can-do attitude, and hard work make me a true asset to any team.",
+
+		
+	],
+};
+
+export const skills = {
+	soft: [
+		{ icon: "👂🏼", text: "Active Listening" },
+		{ icon: "💬", text: "Effective Communication" },
+		{ icon: "👥", text: "Collaboration" },
+		{ icon: "⽓", text: "Teamwork" },
+		{ icon: "💡", text: "Creative Problem Solving" },
+		{ icon: "⌛️", text: "Time management" },
+	],
+	hard: [
+		{ icon: "💻", text: "Scratch" },
+		{ icon: "ʦ", text: " TypeScript" },
+		{ icon: "👥", text: "Effective Teaching" },
+	],
+};
+
+export const professionalData = {
+	title: "Professional Experience",
+	experiences: [
+		{
+			role: "English and Science Teacher, Shamsi Society Model School,Karachi.(2014-2017)",
+			description:
+        
+		    "Teaching English, Science and Social Studies to Primary classes. Handling discipline and attendance Problems.Organizing social and recreational Programs. Counselling students on personal,educational or vocational matters.",
+
+			current: false,
+		},
+		{
+            role: "English and Science Teacher, Kahf Model School,Karachi.(2007-2009)",
+			description:
+		    "Teaching English, Science and Social Studies to grades 1 to 6.",
+            current:false,
+
+		},
+		{
+			role: "Social Studies and Science Teacher, Usman Public School,Karachi.(1997-2006)",
+			description:
+		    "Teaching Social Studies, Science and other subjects to grades 2 to 8.using multiple activities",
+                current:false,
+			},
+			{
+            role:"English and Science Teacher, A.S.F Public School,Karachi.(1996-1997)",
+			description:
+			 "Teaching English, Science and Social Studies to grades 1 to 5.",
+			current: false,
+		},
+	],
+};
